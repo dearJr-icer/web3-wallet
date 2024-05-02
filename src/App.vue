d7fd3ef1caab73ef2103d927d35d6daf2f77f341
icered <iceredlu@gmail.com>
2024-05-02 23:28:49 +0800

    connect eth

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,15 @@
 
 <script setup >
   import {ref} from 'vue';
+  import Web3 from 'web3';
 
   const string = ref('hello web3')
+
+  //实例化web3
+  var web3 = new Web3(Web3.givenProvider || "wss://sepolia.infura.io/ws/v3/81bf523e4ea54e8eb8d9681d909005b1");
+  console.log(web3)
+
+  
 </script>
 
 <styl lang="less">
--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import Vant from 'vant'
-import 'vant/lib/index.css'
+
 
 createApp(App).mount('#app')
-App.use(Vant)
