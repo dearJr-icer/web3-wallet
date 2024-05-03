85b5738abbf591c922b45f81546ed948894346b4
icered <iceredlu@gmail.com>
2024-05-03 09:06:16 +0800

    创建组件

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
   <h3>{{string}}</h3>
   <button type="success">按钮</button>
+
+  <Web3Api></Web3Api>
 </template>
 
 <script setup >
   import {ref} from 'vue';
   import Web3 from 'web3';
+  import Web3Api from './components/Web3Api.vue'
 
   const string = ref('hello web3')
 
@@ -13,6 +16,7 @@
   var web3 = new Web3(Web3.givenProvider || "wss://sepolia.infura.io/ws/v3/81bf523e4ea54e8eb8d9681d909005b1");
   console.log(web3)
 
+  console.log(ref) 
   
 </script>
 
--- src/components/Web3Api.vue
+++ src/components/Web3Api.vue
@@ -1,14 +1,15 @@
-<script setup>
-  import {ref} from 'vue'
-  
-</script>
-
-<template>
-  <div>
-    
-  </div>
-/template>
-
-<style lan="scss">
-  
-</style>
+ <script setup>
+   import {ref} from 'vue'
+   console.log(ref)
+ </script>
+ 
+ <template>
+   <div>
+     api
+   </div>
+ </template>
+ 
+ <style lan="less">
+   
+ </style>
+ 
