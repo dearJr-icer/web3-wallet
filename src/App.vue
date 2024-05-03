ad827e46f9d65a906500163ac1a7593d5183c368
icered <iceredlu@gmail.com>
2024-05-03 09:10:58 +0800

    创建钱包账号

--- src/App.vue
+++ src/App.vue
@@ -7,17 +7,10 @@
 
 <script setup >
   import {ref} from 'vue';
-  import Web3 from 'web3';
+  //import Web3 from 'web3';
   import Web3Api from './components/Web3Api.vue'
 
   const string = ref('hello web3')
-
-  //实例化web3
-  var web3 = new Web3(Web3.givenProvider || "wss://sepolia.infura.io/ws/v3/81bf523e4ea54e8eb8d9681d909005b1");
-  console.log(web3)
-
-  console.log(ref) 
-  
 </script>
 
 <styl lang="less">
--- src/components/Web3Api.vue
+++ src/components/Web3Api.vue
@@ -1,6 +1,18 @@
  <script setup>
-   import {ref} from 'vue'
-   console.log(ref)
+    //import {ref} from 'vue'
+    import Web3 from 'web3';
+
+    //实例化web3
+    var web3 = new Web3(Web3.givenProvider || "wss://sepolia.infura.io/ws/v3/81bf523e4ea54e8eb8d9681d909005b1");
+    console.log(web3)
+
+    //创建账户
+    const account = web3.eth.accounts.create('123') //传入密码
+    console.log(account)
+
+
+
+
  </script>
  
  <template>
