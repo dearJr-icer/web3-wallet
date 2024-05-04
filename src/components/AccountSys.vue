746723af8e8a3e86804fcbcd2beec5d9fa7afe5f
icered <iceredlu@gmail.com>
2024-05-04 15:47:45 +0800

    根据密钥&密码生成keystore

--- src/components/AccountSys.vue
+++ src/components/AccountSys.vue
@@ -2,20 +2,38 @@
     import {ref} from 'vue'
     import * as bip39 from 'bip39'
     import {hdkey} from 'ethereumjs-wallet'
+    import Web3 from 'web3'
+
+
 
     //创建助记词
     /* const mnemonic = bip39.generateMnemonic();
     console.log(mnemonic) */
     const mnemonic = ref('tip uncover virus kick ridge bachelor buzz uniform shaft castle gown blossom')
+    const address = ref(0)
+    const priviteKey = ref(0)
 
     //根据助记词生成密钥对keypair
     const genMnemonic = async () =>{
         const seed =  await bip39.mnemonicToSeed(mnemonic.value)
-        console.log(seed)
         const hdwallet = hdkey.fromMasterSeed(seed)
         // m/purpse'/coin_type'/account'/change/address_index
         const keypair = hdwallet.derivePath("m/44'/60'/0'/0/0")
-        console.log(keypair)
+        //获取私钥
+        const wallet = keypair.getWallet();
+        //获取钱包地址
+        const lowerCaseAddress = wallet.getAddressString()
+        const checkAddress = wallet.getChecksumAddressString()
+        //获取私钥
+        const prikey = wallet.getPrivateKey().toString('hex')
+        address.value = lowerCaseAddress
+        priviteKey.value = prikey
+
+        //导出keystore
+        var web3 = new Web3(Web3.givenProvider || "wss://sepolia.infura.io/ws/v3/81bf523e4ea54e8eb8d9681d909005b1");
+        const keyStore = await web3.eth.accounts.encrypt(`0x${prikey}`,'1111') //参数是  私钥，密码
+        console.log(keyStore)
+
     }
 
     genMnemonic()
@@ -25,6 +43,9 @@
     <div>
       <h1>助记词</h1>
       <p>{{mnemonic}}</p>
+      <p>路径：m/44'/60'/0'/0/0</p>
+      <p>地址：{{address}}</p>
+      <p>秘钥：{{priviteKey}}</p>
     </div>
 </template>
 
